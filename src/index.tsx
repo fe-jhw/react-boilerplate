3b7f1ee29754e5531e9641d9f83262931ece807a
fe-jhw <hyunwoo.jeong@kt.com>
2023-09-14 15:44:39 +0900

    yarn dev msw 사용, 미사용 스크립트 분리

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-if (process.env['NODE_ENV'] === 'development') {
+if (process.env['USE_MSW'] === 'true') {
   const { worker } = require('./mocks/browser')
+  console.log('msw worker start!')
   worker.start()
 }
 
